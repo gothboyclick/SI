dfaca3eb9dd503440720260f3783f7a4ac3cbd09
Adriano Xavier <78619884+gothboyclick@users.noreply.github.com>
2022-10-27 20:45:03 -0300

    BILÉ PAPAIIII

--- firewall.sh
+++ firewall.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
+##conectando a rede interna via bridge
+auto lo
+auto interface
+
+iface interface inet dhcp
+auto interface
+
+## 
+iface interface inet static 
+address 192.168.56.05
+netmask 255.255.255.0
+gateway 192.168.56.200
+
 
 #limpando a  chains
 iptables -F 
 iptables -t nat -F
+
 #PROTGE O FIREWALL#
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
-#proteger de uma rede interna#
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+
 #stateful
 iptables -I INPUT-mstate --state ESTABLISHED,RELATED -j  ACCEPT
 iptables -I OUTPUT -mstate --state ESTABLISHED,RELATED -j ACCEPT
 iptables -I FORWARD -mstate --state ESTABLISHED,RELATED -j ACCEPT
+
 ##permitindo ssh
 iptables -A INPUT -p tcp --dport 22 -s maquinadoprof -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -d maquinadoprof -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -s 192.168.56.x -j ACCEPT
-iptables -A OUTPUT -p tcp- -sport 22 -s 192.168.56.x -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -s 192.168.56.0 -j ACCEPT
+iptables -A OUTPUT -p tcp- -sport 22 -s 192.168.56.0 -j ACCEPT
+
 #acesso rede interna pelo nat
-iptables -t nat -A PREROUTING -p tcp --dports 22  -s 192.168.56.x/24 -o interface -j MASQUERADE
+iptables -t nat -A PREROUTING -s 192.168.56.0/24 -o interface -j MASQUERADE
+
 #liberando rede interna para os servicos
-iptables -A FORWARD -p tcp -m multiport --dports 22 -s 192.168.56.x/24 -d 80,20,25,143 -j ACCEPT
+iptables -A FORWARD -p tcp -m multiport --dports 22 -s 192.168.56.0/24 -d 80,21,25,143 -j ACCEPT
+
 ##liberando pacote icmp
-iptables -A INPUT -p icmp -s 192.168.56.x -j ACCEPT 
-iptables -A OUTPUT -p icmp -s 192.168.56.x -j ACCEPT
-###rede interna para o firewall
-iptables -A INPUT -p tcp --dport 22 -s 192.168.56.x/24 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 22 -d 192.1168.56.x/24 -j ACCEPT
-##
+iptables -A INPUT -p icmp -s 192.168.56.05 -j ACCEPT 
+iptables -A OUTPUT -p icmp -s 192.168.56.05 -j ACCEPT
